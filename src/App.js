69e3a65e2e291c761f871666d54bf84e21ba71d2
Tusar <tusarahmed141@gmail.com>
2023-01-24 16:41:16 +0600

    Practice awesome animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (
-    <section className='main'>
+    <section className="main">
       <div className="custom-container">
-        <div className=' w-96 h-72 border border-slate-300 rounded-md grid place-items-center'>
-          <div className="w-28 h-28 rounded-3xl bg-purple-400"></div>
+        <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center">
+          <motion.div
+            animate={{ x: 70, scale: 1 }}
+            initial={{scale: 0}}
+            className="w-28 h-28 rounded-3xl bg-purple-400"
+          ></motion.div>
         </div>
       </div>
     </section>
