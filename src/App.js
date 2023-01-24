368d6e99ca3de3c5b20a0dd03a92833dee3b0dbd
Tusar <tusarahmed141@gmail.com>
2023-01-24 16:45:26 +0600

    Exploring framer motion react state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const App = () => {
+  const [rotate, setRotate] = useState(false);
   return (
     <section className="main">
       <div className="custom-container">
         <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center">
           <motion.div
-            animate={{ x: 70, scale: 1 }}
-            initial={{scale: 0}}
+            animate={{ rotate: rotate ? 90 : 0, x: rotate ? 70 : 0 }}
+            onClick={() => setRotate(!rotate)}
             className="w-28 h-28 rounded-3xl bg-purple-400"
           ></motion.div>
         </div>
