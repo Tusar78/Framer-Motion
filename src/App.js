62095fa2b7ec130afafc28a88b8f56daac664419
Tusar <tusarahmed141@gmail.com>
2023-01-24 16:57:09 +0600

    Exploring tween type

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center">
           <motion.div
             animate={{ rotate: rotate ? 90 : 0, x: rotate ? 70 : 0 }}
-            transition={{delay: 2}}
+            transition={{type: "tween", duration: .5}}
             onClick={() => setRotate(!rotate)}
             className="w-28 h-28 rounded-3xl bg-purple-400"
           ></motion.div>
