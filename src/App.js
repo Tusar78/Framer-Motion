a8e653a924c725b33ee35188250d69628526f12e
Tusar <tusarahmed141@gmail.com>
2023-01-24 17:00:39 +0600

    Spring method is awesom in framer motion

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const App = () => {
       <div className="custom-container">
         <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center">
           <motion.div
-            animate={{ rotate: rotate ? 90 : 0, x: rotate ? 70 : 0 }}
-            transition={{type: "tween", duration: .5}}
+            animate={{rotate: rotate ? 360: 0, x: rotate ? 100 : -100 }}
+            transition={{type: "spring", duration: .5}}
             onClick={() => setRotate(!rotate)}
             className="w-28 h-28 rounded-3xl bg-purple-400"
           ></motion.div>
