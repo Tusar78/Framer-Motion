8be3b2b7ba092493f018e084de1128996a678dd7
Tusar <tusarahmed141@gmail.com>
2023-01-24 16:33:28 +0600

    Example ready for framer motion

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 const App = () => {
   return (
     <section className='main'>
-      <div className="container">
-        <div className=" w-28 h-28 bg-purple-400 rounded-3xl"></div>
+      <div className="custom-container">
+        <div className=' w-96 h-72 border border-slate-300 rounded-md grid place-items-center'>
+          <div className="w-28 h-28 rounded-3xl bg-purple-400"></div>
+        </div>
       </div>
     </section>
   );
