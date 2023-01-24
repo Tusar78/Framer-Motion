f2c2774d6ab0c6dcc152c0d26b669eb60abb6e64
Tusar <tusarahmed141@gmail.com>
2023-01-24 17:27:52 +0600

    Drag constrains are also awesome

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ const App = () => {
   return (
     <section className="main">
       <div className="custom-container">
-        <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center overflow-hidden">
+        {/* <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center overflow-hidden">
           <motion.div
             animate={{rotate: rotate ? 360: 0, x: rotate ? 100 : -100 }}
             transition={{type: "tween", velocity: 40, duration: .5}}
             whileHover={{scale: 1.2}}
             whileDrag=""
             drag
+            onClick={() => setRotate(!rotate)}
+            className="w-28 h-28 rounded-3xl bg-purple-400"
+          ></motion.div>
+        </div> */}
+        <div className=" w-6/12 h-96 border border-slate-300 rounded-md grid place-items-center overflow-hidden">
+          <motion.div
+            whileDrag={{scale: 1.2}}
+            drag
+            dragConstraints={{left: 50}}
             onClick={() => setRotate(!rotate)}
             className="w-28 h-28 rounded-3xl bg-purple-400"
           ></motion.div>
