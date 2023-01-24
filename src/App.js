b25b3a314b02ffc5c75a4684b8a3f4a3df998963
Tusar <tusarahmed141@gmail.com>
2023-01-24 16:17:48 +0600

    React create enviroment ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='main'>
+      <div className="container">
+        <div className=" w-28 h-28 bg-purple-400 rounded-3xl"></div>
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
