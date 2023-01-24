401f5234d69f0052fe5118155c8302ab5628ddd3
Tusar <tusarahmed141@gmail.com>
2023-01-24 17:02:17 +0600

    All of animation are awesome

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <div className=" w-96 h-72 border border-slate-300 rounded-md grid place-items-center">
           <motion.div
             animate={{rotate: rotate ? 360: 0, x: rotate ? 100 : -100 }}
-            transition={{type: "spring", duration: .5}}
+            transition={{type: "keyframes", duration: .5}}
             onClick={() => setRotate(!rotate)}
             className="w-28 h-28 rounded-3xl bg-purple-400"
           ></motion.div>
